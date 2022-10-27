9d8bd66cbfb19c62e9bf1653c3bb523e83b8b972
Rnoppers <104368256+Rnoppers@users.noreply.github.com>
2022-10-27 12:06:59 +0200

    Commit for working branch

--- Mankala/Program.cs
+++ Mankala/Program.cs
@@ -6,7 +6,7 @@ namespace Mankala
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("NIeuweBranch");
         }
     }
 }
